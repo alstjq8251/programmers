4ded5bf39d1430287af049d60acafe9cded06aa7
kim min seop <98382954+alstjq8251@users.noreply.github.com>
2023-03-05 16:17:29 +0900

    Update 숫자 변환하기.java

--- 문제/day76/숫자 변환하기.java
+++ 문제/day76/숫자 변환하기.java
@@ -2,37 +2,30 @@ import java.util.*;
 class Solution {
     class Nm{
         int x;
-        int y;
         int loop;
-        public Nm(int x, int y, int loop){
+        public Nm(int x, int loop){
             this.x = x;
-            this.y = y;
             this.loop = loop;
         }
     }
     Map<Integer,Boolean> map = new HashMap<>();
     public int solution(int x, int y, int n) {
-        if(y % x != 0 && y-n <x){
-            return -1;
-        }
         int answer =0;
         answer = bfs(x,y,n);
         return answer;
     }
     private int bfs(int x, int y, int n){
         Queue<Nm> q = new LinkedList<>();
-        q.offer(new Nm(x,y,0));
+        q.offer(new Nm(x,0));
         while(!q.isEmpty()){
             Nm nm = q.poll();
-            if(nm.x == nm.y){
+            if(nm.x == y){
                 return nm.loop;
-            }else if(nm.x >y){
-                break;
             }
             for(int i=0; i<3; i++){
                 int postx = discriminate(nm.x,i,n);
                 if(postx <=y && !map.getOrDefault(postx,false)){
-                    q.offer(new Nm(postx,y,nm.loop+1));
+                    q.offer(new Nm(postx,nm.loop+1));
                     map.put(postx,true);
                 }
             }
